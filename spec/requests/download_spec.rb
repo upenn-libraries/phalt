19cd9b5288bd42bc4917d202f552a94d4b34e398
Mike Kanning <mkanning@upenn.edu>
2021-06-07 15:44:46 -0400

    add specs and fix discovered bugs in new extension handling

--- spec/requests/download_spec.rb
+++ spec/requests/download_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe 'Download Requests', type: :request do
 
   context 'when file is available' do
     let(:bucket) { 'cool_bucket' }
-    let(:file) { 'best_image.jpeg' }
+    # let(:file) { 'best_image.jpeg' }
     let(:image) { File.join('spec', 'fixtures', 'puppy.jpg') }
     let(:headers) do
       {
         'Last-Modified' => 'Sun, 15 Apr 2018 10:50:19 GMT',
-        'ETag' => '989817aabd5f2da6a1c77425a348a080',
+        'ETag' => '989817aabd5f2da6a1c77425a348a080'
       }
     end
 
@@ -30,40 +30,69 @@ RSpec.describe 'Download Requests', type: :request do
         .to_return(status: 200, headers: headers, body: File.new(image)).times(1)
     end
 
-    context 'when neither filename nor disposition are provided' do
-      before { get "/download/#{bucket}/#{file}" }
+    context 'with a filename with one extensions' do
+      let(:file) { 'best_image.jpeg' }
 
-      it { expect(last_response.status).to be 200 }
-      it { expect(last_response.headers['Content-Disposition']).to eql 'attachment; filename="best_image.jpeg"' }
-      it { expect(last_response.headers['Content-Type']).to eql 'image/jpeg' }
-      it { expect(last_response.headers['Content-Length']).to eql '17037' }
-      it { expect(last_response.headers['Last-Modified']).to eql 'Sun, 15 Apr 2018 10:50:19 GMT'}
-      it { expect(last_response.headers['ETag']).to eql '989817aabd5f2da6a1c77425a348a080' }
+      context 'when neither filename nor disposition are provided' do
+        before { get "/download/#{bucket}/#{file}" }
 
-      it 'returns image as body' do
-        expect(last_response.body).to eql File.read(image)
+        it { expect(last_response.status).to be 200 }
+        it { expect(last_response.headers['Content-Disposition']).to eql 'attachment; filename="best_image.jpeg"' }
+        it { expect(last_response.headers['Content-Type']).to eql 'image/jpeg' }
+        it { expect(last_response.headers['Content-Length']).to eql '17037' }
+        it { expect(last_response.headers['Last-Modified']).to eql 'Sun, 15 Apr 2018 10:50:19 GMT'}
+        it { expect(last_response.headers['ETag']).to eql '989817aabd5f2da6a1c77425a348a080' }
+
+        it 'returns image as body' do
+          expect(last_response.body).to eql File.read(image)
+        end
+      end
+
+      context 'when filename is provided' do
+        before { get "/download/#{bucket}/#{file}?filename=new_file" }
+
+        it { expect(last_response.status).to be 200 }
+        it { expect(last_response.headers['Content-Disposition']).to eql 'attachment; filename="new_file.jpeg"' }
+      end
+
+      context 'when disposition is provided' do
+        before { get "/download/#{bucket}/#{file}?disposition=inline" }
+
+        it { expect(last_response.status).to be 200 }
+        it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="best_image.jpeg"' }
+      end
+
+      context 'when filename and disposition are provided' do
+        before { get "/download/#{bucket}/#{file}?disposition=inline&filename=new_file" }
+
+        it { expect(last_response.status).to be 200 }
+        it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="new_file.jpeg"' }
+      end
+
+      context 'when filename param includes an extension' do
+        before { get "/download/#{bucket}/#{file}?disposition=inline&filename=best_image.jpeg" }
+
+        it { expect(last_response.status).to be 500 }
+        it { expect(last_response.body).to eq 'Don\'t include an extension in the filename param' }
       end
     end
 
-    context 'when filename is provided' do
-      before { get "/download/#{bucket}/#{file}?filename=new_file" }
+    context 'with a filename param with multiple extensions' do
+      let(:file) { 'best_archive123456.warc.gz' }
+
+      before { get "/download/#{bucket}/#{file}?disposition=inline&filename=best_archive" }
 
       it { expect(last_response.status).to be 200 }
-      it { expect(last_response.headers['Content-Disposition']).to eql 'attachment; filename="new_file.jpeg"' }
+      it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="best_archive.warc.gz"' }
     end
 
-    context 'when disposition is provided' do
-      before { get "/download/#{bucket}/#{file}?disposition=inline" }
+    context 'with a filename param containing a period' do
+      let(:file) { 'best.image123456.jpeg' }
+
+      before { get "/download/#{bucket}/#{file}?disposition=inline&filename=best.image" }
 
       it { expect(last_response.status).to be 200 }
-      it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="best_image.jpeg"' }
-    end
-
-    context 'when filename and disposition are provided' do
-      before { get "/download/#{bucket}/#{file}?disposition=inline&filename=new_file" }
-
-      it { expect(last_response.status).to be 200 }
-      it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="new_file.jpeg"' }
+      it { expect(last_response.headers['Content-Disposition']).to eql 'inline; filename="best.image.jpeg"' }
     end
   end
 
